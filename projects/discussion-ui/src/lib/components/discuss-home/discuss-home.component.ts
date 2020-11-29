df2c905c23c00e93ffb6ca2f887dd7dc2eb27ea1
vinukumar-vs <vinu.kumar@tarento.com>
2020-11-29 10:13:31 +0530

    API response JSON has defined as costatnt

--- projects/discussion-ui/src/lib/components/discuss-home/discuss-home.component.ts
+++ projects/discussion-ui/src/lib/components/discuss-home/discuss-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Constants from '../../common/constants.json';
 
 @Component({
   selector: 'lib-discuss-home',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class DiscussHomeComponent implements OnInit {
 
   constructor() { }
+  
+  category: any = (Constants as any).default.categories[0];
 
   ngOnInit() {
   }
